ed19d5f626bc03905d8c01c43321da50696ba483
Alex Schaeffer <AMVPh34r@gmail.com>
2016-05-09 17:13:29 -0400

    Add favicon and meta logo

--- view/_include/html_header.php
+++ view/_include/html_header.php
@@ -2,6 +2,12 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
+		<link rel="shortcut icon" sizes="16x16 32x32 48x48 64x64" href="/img/favicon.ico">
+		<link rel="shortcut icon" type="image/x-icon" href="/img/favicon.ico">
+		<!--[if IE]><link rel="shortcut icon" href="/img/favicon.ico"><![endif]-->
+		<meta property="og:title" content="<?php echo CONFIG::$sSiteTitle; ?>" />
+		<meta property="og:image" content="/img/logo-a.png" />
+
 		<title><?php echo CONFIG::$sSiteTitle; ?></title>
 		<link rel="stylesheet" href="/css/style.css" />
 	</head>
