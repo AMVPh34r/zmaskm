aad67cda28ba00959a1f1745303a3f07ff068046
Alex Schaeffer <AMVPh34r@gmail.com>
2016-05-09 16:59:57 -0400

    Add social links, move about text to template

---
+++ view/_include/index/about.php
@@ -0,0 +1,8 @@
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec nisi leo. Nam feugiat lorem sem, sit amet ultricies erat mattis sit amet. Suspendisse potenti. Nam finibus gravida magna, non malesuada ipsum commodo sit amet. Maecenas faucibus bibendum vehicula. Quisque non sodales lectus. Praesent elementum augue at egestas elementum. Fusce in blandit magna. Morbi auctor consequat sapien, nec dapibus sapien rutrum bibendum. Suspendisse potenti. Ut a maximus est, vitae rutrum ipsum. Aliquam vulputate velit id dapibus sagittis. Vestibulum at viverra nibh. Vivamus tempus velit sed erat interdum, nec mollis libero pharetra.</p>
+<div class="social">
+	<ul>
+		<li><a href="https://twitter.com/zMASKm" target="_blank"><img src="/img/social/icon-twitter.png" alt="Twitter" title="Mask on Twitter"></a></li>
+		<li><a href="https://twitch.tv/zMASKm/profile" target="_blank"><img src="/img/social/icon-twitch.png" alt="Twitch" title="Mask on Twitch"></a></li>
+		<li><a href="http://steamcommunity.com/id/zMASKm" target="_blank"><img src="/img/social/icon-steam.png" alt="Steam" title="Mask on Steam"></a></li>
+	</ul>
+</div>
--- view/page/index.php
+++ view/page/index.php
@@ -7,7 +7,7 @@
 			<img src="img/logo.png" />
 		</div>
 		<div class="content text" data-name="about">
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec nisi leo. Nam feugiat lorem sem, sit amet ultricies erat mattis sit amet. Suspendisse potenti. Nam finibus gravida magna, non malesuada ipsum commodo sit amet. Maecenas faucibus bibendum vehicula. Quisque non sodales lectus. Praesent elementum augue at egestas elementum. Fusce in blandit magna. Morbi auctor consequat sapien, nec dapibus sapien rutrum bibendum. Suspendisse potenti. Ut a maximus est, vitae rutrum ipsum. Aliquam vulputate velit id dapibus sagittis. Vestibulum at viverra nibh. Vivamus tempus velit sed erat interdum, nec mollis libero pharetra.</p>
+			<?php Template::includeTemplate('index/about.php') ?>
 		</div>
 	</div>
 </div>
